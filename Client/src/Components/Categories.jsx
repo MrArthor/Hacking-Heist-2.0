35877e4be98bd2f60da394fe1caae935656297d7
Vansh Sachdev <vksvs25@gmail.com>
2022-06-26 13:10:18 +0530

    Update

--- Client/src/Components/Categories.jsx
+++ Client/src/Components/Categories.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
+import React, {  } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios';
 
+import Science from "../assets/images/Science.jpg"
+import culture from "../assets/images/culture.jpg"
+import career from "../assets/images/career.jpg"
+import fashion from "../assets/images/fashion.jpg"
+import politics from "../assets/images/politics.png"
 
-import banner4 from "../assets/images/banner4.jpg"
-import banner5 from "../assets/images/banner5.jpg"
-import banner6 from "../assets/images/banner6.jpg"
-import banner7 from "../assets/images/banner7.jpg"
-import banner8 from "../assets/images/banner8.jpg"
 import "../assets/style/style.css"
 const api = axios.create({
     baseURL: 'http://localhost:9483/',
@@ -33,7 +33,7 @@ console.log(Data);
 
 
 export default function Categories() {
-    const categories = useState(Data);
+    // const categories = useState(Data);
     return (
         <>
             <br/><br/><br/>
@@ -41,7 +41,7 @@ export default function Categories() {
             <div className='col'>
                 <div className='row-xxl-1 row-xl-1 row-lg-1 row-md-1 row-sm-1 row-1'>
                     <div className="card">
-                        <img src={banner4} className="card-img-top image" alt="..."/>
+                        <img src={Science} className="card-img-top image" alt="..."/>
                             <div className="card-body">
                                 <h5 className="card-title">Science</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -52,7 +52,7 @@ export default function Categories() {
                 
                 <div className='row-xxl-1 row-xl-1 row-lg-1 row-md-1 row-sm-1 row-1'>
                     <div className="card">
-                        <img src={banner5} className="card-img-top image" alt="..."/>
+                        <img src={politics} className="card-img-top image" alt="..."/>
                             <div className="card-body">
                                 <h5 className="card-title">Politics</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -61,7 +61,7 @@ export default function Categories() {
                     </div>
                 </div><div className='row-xxl-1 row-xl-1 row-lg-1 row-md-1 row-sm-1 row-1'>
                     <div className="card">
-                        <img src={banner6} className="card-img-top image" alt="..."/>
+                        <img src={culture} className="card-img-top image" alt="..."/>
                             <div className="card-body">
                                 <h5 className="card-title">Culture and Trends</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -71,7 +71,7 @@ export default function Categories() {
                 </div>
                 <div className='row-xxl-1 row-xl-1 row-lg-1 row-md-1 row-sm-1 row-1'>
                     <div className="card">
-                        <img src={banner7} className="card-img-top image" alt="..."/>
+                        <img src={fashion} className="card-img-top image" alt="..."/>
                             <div className="card-body">
                                 <h5 className="card-title">Fashion</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -81,7 +81,7 @@ export default function Categories() {
                 </div>
                 <div className='row-xxl-1 row-xl-1 row-lg-1 row-md-1 row-sm-1 row-1'>
                     <div className="card">
-                        <img src={banner8} className="card-img-top image" alt="..."/>
+                        <img src={career} className="card-img-top image" alt="..."/>
                             <div className="card-body">
                                 <h5 className="card-title">Career</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
